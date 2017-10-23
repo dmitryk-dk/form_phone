25604a98ff85ca94c411c43298db997ec26aaef9
Vyacheslav Pryimak <vyacheslavpryimak@gmail.com>
2017-10-23 14:18:34 +0300

    getHostFn refactor

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 export const getPhones = 'phones';
 export const postPhone = 'phone';
 export const delPhone = 'delete';
-export const getHostFn = function () {
+export const getHostFn = () => {
     return window.location.protocol+"//"+window.location.host+"/{API}"
 }
